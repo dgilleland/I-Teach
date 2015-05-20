4a56ed847b0e3f0806fbf0b16e9d2306339c29ec
Dan Gilleland <dgilleland@nait.ca>
2015-05-20 13:25:54 -0600

    Initial class library projects w. specs
    I-Teach.CoursePlanningCalendar
    I-Teach.CoursePlanningCalendar.Specs
    with initial command tests and comments on some acceptance tests

---
+++ src/I-Teach.CoursePlanningCalendar.Specs/Helpers/NullOrWhiteSpaceTestData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Xunit;
+namespace I_Teach.CoursePlanningCalendar.Specs.Helpers
+{
+    public class NullOrWhiteSpaceTestData : TheoryData<string>
+    {
+        //public IEnumerator<string> GetEnumerator()
+        //{
+        //    yield return null;
+        //    yield return "";
+        //    yield return " ";
+        //    yield return "  ";
+        //    yield return "\t";
+        //    yield return "\n";
+        //}
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    return GetEnumerator();
+        //}
+    }
+}
---
+++ src/I-Teach.CoursePlanningCalendar.Specs/UnitTests/Commands/CreatePlanningCalendar_Command.cs
@@ -0,0 +1,44 @@
+﻿using I_Teach.CoursePlanningCalendar.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace I_Teach.CoursePlanningCalendar.Specs.UnitTests.Commands
+{
+    public class CreatePlanningCalendar_Command
+    {
+        #region Theory test
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("  ")]
+        [InlineData("\t")]
+        [InlineData("\n")]
+        [Trait("Context", "Planning Calendar Commands")]
+        #endregion
+        public void Should_Reject_An_Empty_Course_Name(string name)
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.Throws<ArgumentException>(() => { new CreatePlanningCalendar(name, "ABCD1001"); });
+        }
+
+        #region Theory test
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("  ")]
+        [InlineData("\t")]
+        [InlineData("\n")]
+        [Trait("Context", "Planning Calendar Commands")]
+        #endregion
+        public void Should_Reject_An_Empty_Course_Number(string number)
+        {
+            Assert.Throws<ArgumentException>(() => { new CreatePlanningCalendar("Introductory Unit Testing", number); });
+        }
+    }
+}
