de7a36cd30c6e813f08e66b0328f5c9a1b9a92d8
Dan Gilleland <dgilleland@nait.ca>
2015-05-22 14:56:53 -0600

    Separate solution for Planning Calendar

--- src/I-Teach/Properties/AssemblyInfo.cs
+++ src/I-Teach/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("I-Teach")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © DMIT 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
