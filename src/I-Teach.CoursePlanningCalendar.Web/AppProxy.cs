74b68bdf49b71731bd64b109844781104b4e6e23
Dan Gilleland <dgilleland@nait.ca>
2015-08-27 15:20:29 -0600

    AdHoc website for faster prototyping

---
+++ src/I-Teach.CoursePlanningCalendar.Web/AppProxy.cs
@@ -0,0 +1,29 @@
+﻿using I_Teach.CoursePlanningCalendar.Fetch.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace I_Teach.CoursePlanningCalendar.Web
+{
+    public class AppProxy
+    {
+        public I_Teach.SchoolApplication App { get; set; }
+        public AppProxy()
+        {
+            App = I_Teach.SchoolApplication.Instance();
+        }
+
+        public static AppProxy Instance()
+        {
+            return new AppProxy();
+        }
+
+        public IEnumerable<DraftPlanningCalendar> ListCalendars()
+        {
+            var calendars = App.PlanningCalendarRepository.ListDraftPlanningCalendars();
+
+            return calendars;
+        }
+    }
+}
