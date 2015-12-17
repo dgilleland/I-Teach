37d48877214c2aaaf360049b8b42a996bc09a1f2
dagilleland <dagilleland@shaw.ca>
2015-12-17 08:52:56 -0700

    Framing out initial domain model

---
+++ src/I-Teach.ClassMarks.Specs/UnitTests/Course_Entity_Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Teach.ClassMarks.Specs.UnitTests
+{
+    public class Course_Entity_Model
+    {
+    }
+    public class Student_Entity_Model
+    {
+
+    }
+    public class StudentId_Value_Type
+    {
+
+    }
+
+}
---
+++ src/I-Teach.ClassMarks/Models/Class.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Teach.ClassMarks.Models
+{
+    internal class Class
+    {
+        private Class(CourseSection section, Term term, EvaluationComponents evaluations)
+        {
+        }
+
+        public static Class CreateClass(CourseSection section, Term term, EvaluationComponents evaluations)
+        {
+            return new Class(section, term, evaluations);
+        }
+
+        private void AddStudent(Student student)
+        {
+            
+        }
+
+        private void RemoveStudent(StudentId studentId)
+        {
+            
+        }
+
+        private void TransferStudent(StudentId studentId, Class section)
+        {
+            
+        }
+
+        private void AdjustCourseComponent()
+        {
+            
+        }
+
+        private void EnterMark(StudentId studentId, Evaluation evaluationItem, Mark mark)
+        {
+            
+        }
+    }
+}
